89b05332292e6061618f53ba7d288779a572b123
Zackary Liel <47030040+zliel@users.noreply.github.com>
2024-10-29 12:35:42 -0700

    Mapped the demonstration views to url endpoints

--- demo/urls.py
+++ demo/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
 
+from .views import hello, hello_user, hello_return_dictionary
+
+# Here we map urls to specific views
+# These urls are added to the main urls.py file in the QuizWhiz_backend folder
 urlpatterns = [
-    path("", )
+    path("", hello),
+    path("<str:username>", hello_user),
+    # Be careful with the order of the paths. The one with the variable should be at the end
+    path("test/dictionary", hello_return_dictionary),
+
 ]
