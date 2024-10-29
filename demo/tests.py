f60152e4b89c8563b61f332b37f6b4290726eb1e
Zackary Liel <47030040+zliel@users.noreply.github.com>
2024-10-29 12:46:27 -0700

    Added a few tests for API endpoints for "/demo"

--- demo/tests.py
+++ demo/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
 
+from rest_framework.test import APITestCase, APIClient
+
+
 # Create your tests here.
+# Showing how to test API endpoints
+class TestHelloWorld(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_hello_world(self):
+        response = self.client.get('/demo/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, 'Hello, world!')
+
+    def test_hello_user(self):
+        response = self.client.get('/demo/User')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, 'Hello, User!')
+
+    def test_hello_dictionary(self):
+        response = self.client.get('/demo/test/dictionary')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, {"My custom Message Name": "Hello, world!"})
