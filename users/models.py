5030284d1ec65c6aa77574f6d0af9004b6c29a49
Zackary Liel <47030040+zliel@users.noreply.github.com>
2024-11-08 10:43:25 -0800

    Removed the profile_picture field for now

--- users/models.py
+++ users/models.py
@@ -6,6 +6,5 @@ from django.db import models
 class User(AbstractUser):
     # AbstractUser is a built-in Django model that provides a lot of the functionality we need for a user
     # So here we'll just add some additional information that we may or may not use
-    profile_picture = models.ImageField(upload_to="profile_pictures/", null=True, blank=True)
     bio = models.TextField(null=True, blank=True, max_length=500)
     google_id = models.CharField(max_length=255, null=True, blank=True)
