3b1bd4e93274d475173a0ba1b39d0bc96936d888
EAdibi <adibielina@gmail.com>
2024-11-12 11:57:45 -0800

    Set up swagger docs at the "/docs" endpoint

--- QuizWhiz_backend/urls.py
+++ QuizWhiz_backend/urls.py
@@ -16,14 +16,31 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from allauth import urls as auth_urls
 
 from demo import urls as demo_urls
 # from users import urls as users_urls
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 urlpatterns = [
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
     path('demo/', include(demo_urls)),
     path('auth/', include(auth_urls)),
     # path('users/', include(users_urls)),
 ]
+
