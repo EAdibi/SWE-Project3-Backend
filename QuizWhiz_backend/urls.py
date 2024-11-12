15537401fc05d6c834f898df54a6ddd65355faf6
EAdibi <adibielina@gmail.com>
2024-11-12 12:03:10 -0800

    Updated the schema info

--- QuizWhiz_backend/urls.py
+++ QuizWhiz_backend/urls.py
@@ -26,12 +26,9 @@ from demo import urls as demo_urls
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="QuizWhiz API",
       default_version='v1',
-      description="Test description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
+      description="A simple API for creating and managing flashcards",
    ),
    public=True,
    permission_classes=(permissions.AllowAny,),
