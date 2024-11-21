c42576a1f369557e3176e2e645c71de9eb38abc8
EAdibi <adibielina@gmail.com>
2024-11-21 11:30:58 -0800

    Lesson model

---
+++ lessons/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+class Lesson(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    category = models.CharFields(max_length=64)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='lessons') #link to the user who created the lesson
+    is_public = models.BooleanField(default=False)
