80b97a2bd627e51a2934d35a9db7ebe3581613d2
Zackary Liel <47030040+zliel@users.noreply.github.com>
2024-12-05 13:23:02 -0800

    Added a view for getting all public flashcards

--- flashcards/urls.py
+++ flashcards/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import FlashcardDetailView, FlashcardCreateView
+from .views import FlashcardDetailView, FlashcardCreateView,FlashcardListView
 
 urlpatterns = [
     path('<int:id>/', FlashcardDetailView.as_view(), name='flashcard-detail'),  # For GET, PUT, DELETE
     path('', FlashcardCreateView.as_view(), name='flashcard-create'),  # for POST
+    path('public/', FlashcardListView.as_view(), name='flashcard-list'),  # for GET
 ]
